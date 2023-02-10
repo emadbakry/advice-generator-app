d40e1f718825047d9033139e6cb3d74ce95d7ac8
emadbakry <101658737+emadbakry@users.noreply.github.com>
2023-02-10 08:05:12 +0300

    advice_generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import disc_icon from "./images/icon-dice.svg";
+import pattern_icon from "./images/pattern-divider-mobile.svg";
+import pattern_icon_desk from "./images/pattern-divider-desktop.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <div className="section">
+      <div className="top">
+        <h1>
+          Advice #<span id="advice_counter">0</span>
+        </h1>
+        <div className="advice_container">
+        <p id="advice">Loading...</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="line">
+          <img src={pattern_icon} alt="icon" className="mob_img" />
+          <img src={pattern_icon_desk} alt="icon" className="desk_img" />
+        </div>
+        <div className="generate_advice">
+          <button type="button" id="generate_btn" className="content_loaded">
+            <img src={disc_icon} alt="icon" />
+          </button>
+        </div>
+      </div>
+      </div>
+      <div className='attr'>
+      By <a href="https://emadcode.com">Emadcode.com</a>
+      </div>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -15,3 +15,50 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// All scripts for the advice-generator app
+function run_script() {
+  let advice_copy = '';
+  const advice_text = document.getElementById('advice');
+  const advice_counter = document.getElementById('advice_counter');
+  async function gen_advice() {
+    // show downloading icon
+    document.body.classList.add('loading');
+    await fetch("https://api.adviceslip.com/advice")
+    .then((i) => i.json())
+      .then((i) => {
+        setTimeout(() => {
+          if (advice_copy !== i.slip.advice) {
+            advice_counter.textContent = Number(advice_counter.textContent) + 1;
+            advice_text.textContent = i.slip.advice;
+            // hide downloading icon
+            document.body.classList.remove("loading");
+            advice_copy = i.slip.advice;
+          } else {
+            console.log("same");
+            gen_advice();
+          }
+      }, 200);
+       });
+  }
+  gen_advice();
+  const gen_btn = document.getElementById("generate_btn");
+  gen_btn.addEventListener("click", () => {
+    if (!document.body.classList.contains('loading')) {
+      gen_advice()
+    }
+  });
+
+} // End generating advice codes
+
+// dont edit below
+let my_interval = setInterval(() => {
+  let content = document.querySelector("#generate_btn").className;
+  console.log('content not yet')
+  if (content === "content_loaded") {
+    console.log('content loaded now');
+    run_script();
+    clearInterval(my_interval);
+  }
+  
+}, 200);
